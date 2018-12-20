b1eb9b7dd29111b8a849a3d7481abb9b188298df
addil <aadil01@hotmail.co.uk>
2018-12-20 19:04:32 +0000

    Added area filter option.

--- Zoopla/filters.py
+++ Zoopla/filters.py
@@ -1,8 +1,11 @@
 import django_filters, json
+from django.db.models import Q
+
 from Zoopla.models import Property
 
 
 class PropertyBasicFilter(django_filters.FilterSet):
+    area = django_filters.CharFilter(method="filter_area")
     price = django_filters.RangeFilter(field_name="rentalprice__per_month") # price_min and price_max
     num_bathrooms = django_filters.RangeFilter()
     num_bedrooms = django_filters.RangeFilter()
@@ -22,8 +25,11 @@ class PropertyBasicFilter(django_filters.FilterSet):
     #
     #     return qs
 
+    def filter_area(self, qs, field, value):
+        return qs.filter(Q(outcode__istartswith=value) | Q(post_town__iexact=value))
+
     class Meta:
         model = Property
-        fields = ('price', 'listing_status', 'property_type',
+        fields = ('area', 'price', 'listing_status', 'property_type',
                   'num_bathrooms', 'num_bedrooms', 'num_floors',
                   'num_recepts')
