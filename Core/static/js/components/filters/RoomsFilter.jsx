71c78e95c9704c41dcf109e56bc724aef7c8d1c1
addil <aadil01@hotmail.co.uk>
2019-01-26 18:40:21 +0000

    Updated rooms filter

--- Core/static/js/components/filters/RoomsFilter.jsx
+++ Core/static/js/components/filters/RoomsFilter.jsx
@@ -20,9 +20,9 @@ export default class RoomsFilter extends BaseFilter {
     constructor(props) {
         super(props);
 
-        this.state.bedrooms = 7;
-        this.state.bathrooms = 3;
-        this.state.receptions = 1;
+        this.state.bedrooms = {min: 3, max: 4};
+        this.state.bathrooms = {min: 1, max: 2};
+        this.state.receptions = {min: 1, max: 1};
     }
 
     static description = "Select the number of each type of room needed.";
@@ -39,12 +39,13 @@ export default class RoomsFilter extends BaseFilter {
     };
 
     getCollapsedText = () => {
+        const {bedrooms, bathrooms, receptions} = this.state;
         return (
             <Fragment>
                 <h3>Rooms</h3>
-                <p><i className="fas fa-bed"/> Bedrooms {this.state.bedrooms}</p>
-                <p><i className="fas fa-toilet"/> Bathrooms {this.state.bathrooms}</p>
-                {this.state.receptions > 0 && <p><i className="fas fa-couch"/> Receptions {this.state.bathrooms}</p>}
+                {bedrooms.min && <p><i className="fas fa-bed"/> Bedrooms {bedrooms.min}-{bedrooms.max}</p>}
+                {bathrooms.min && <p><i className="fas fa-toilet"/> Bathrooms {bathrooms.min}-{bathrooms.max}</p>}
+                {receptions.min && <p><i className="fas fa-couch"/> Receptions {receptions.min}-{receptions.max}</p>}
             </Fragment>
         )
     };
@@ -58,12 +59,12 @@ export default class RoomsFilter extends BaseFilter {
                 <Header style={{marginTop: 0}} size='small'><i className="fas fa-bed"/> How many bedrooms do you
                     need?</Header>
                 <Range
-                    defaultValue={[1, 10]}
+                    defaultValue={bedrooms.min ? [bedrooms.min, bedrooms.max] : [1, 10]}
                     step={1}
                     min={1}
                     max={10}
                     marks={bedroomsRange()}
-                    onChange={(a) => this.setState({data: {...this.state.data, num_bedrooms_min: a}})}
+                    onChange={(a) => this.setState({bedrooms: {min: a[0], max: a[1]}})}
                 />
 
                 <br/>
@@ -71,12 +72,12 @@ export default class RoomsFilter extends BaseFilter {
                 <Header style={{marginTop: 0}} size='small'><i className="fas fa-toilet"/> How many bathrooms do
                     you need?</Header>
                 <Range
-                    defaultValue={[1, 10]}
+                    defaultValue={bathrooms.min ? [bathrooms.min, bathrooms.max] : [1, 10]}
                     step={1}
                     min={1}
                     max={10}
                     marks={bedroomsRange()}
-                    onChange={(a) => this.setState({data: {...this.state.data, num_bedrooms_min: a}})}
+                    onChange={(a) => this.setState({bathrooms: {min: a[0], max: a[1]}})}
                 />
 
                 <br/>
@@ -84,12 +85,12 @@ export default class RoomsFilter extends BaseFilter {
                 <Header style={{marginTop: 0}} size='small'><i className="fas fa-couch"/> How many reception
                     areas do you need?</Header>
                 <Range
-                    defaultValue={[1, 10]}
+                    defaultValue={receptions.min ? [receptions.min, receptions.max] : [1, 5]}
                     step={1}
                     min={1}
                     max={10}
                     marks={bedroomsRange()}
-                    onChange={(a) => this.setState({data: {...this.state.data, num_bedrooms_min: a}})}
+                    onChange={(a) => this.setState({receptions: {min: a[0], max: a[1]}})}
                 />
                 <br/>
             </Fragment>
